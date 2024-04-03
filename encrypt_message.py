6bf0f3611adf770ceddfc5f1bd4c74f2ad99a315
Nolan Patterson <ntpatter@iupui.edu>
2024-04-03 17:06:47 -0400

    working encrypt_message.py

--- encrypt_message.py
+++ encrypt_message.py
@@ -3,28 +3,32 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.backends import default_backend
 import base64
 
+
 # Helper method to convert bytes to strings
 def utf8(s: bytes):
     return str(s, 'utf-8')
+
 
 # Load public key from external file
 def load_public_key(file_name):
     with open(file_name, 'rb') as file:
         public_key_data = file.read()
-        public_key = serialization.load_pem_public_key(public_key_data, backend=default_backend())
+        public_key = serialization.load_pem_public_key(
+            public_key_data, backend=default_backend())
     return public_key
+
 
 # Helper method to encrypt message
 def encrypt_message(msg, key):
     encrypted_msg = base64.b64encode(key.encrypt(
-        msg, 
+        msg,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None
         )
     ))
-    return encrypted_msg # Returns the bytes
+    return encrypted_msg  # Returns the bytes
 
 
 def main():
@@ -32,12 +36,17 @@ def main():
     public_key = load_public_key('my_public_key.pem')
 
     # Create a secret message and print it to the screen
+    message = b"Hello, this is a secret message!"
+    print(f"Message: {message}")
 
     # Encrypt your message
+    encrypted_message = encrypt_message(message, public_key)
+    print(f"Encrypted Message: {encrypted_message}")
 
     # Write the encrypted message to a file
+    with open('encrypted_message.bin', 'wb') as file:
+        file.write(encrypted_message)
 
-    pass
 
 if __name__ == "__main__":
     main()
